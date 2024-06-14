f274b1171a63a82ded2208acd29924ab0b3aac96
Cornelis-tech <124459319+Cornelis-tech@users.noreply.github.com>
2024-06-14 03:47:08 +0000

    ew changes

--- src/app/markdown/markdown.component.spec.ts
+++ src/app/markdown/markdown.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CreateMarkdownComponent } from './create-markdown.component';
+import { MarkdownComponent } from './markdown.component';
 
-describe('CreateMarkdownComponent', () => {
-  let component: CreateMarkdownComponent;
-  let fixture: ComponentFixture<CreateMarkdownComponent>;
+describe('MarkdownComponent', () => {
+  let component: MarkdownComponent;
+  let fixture: ComponentFixture<MarkdownComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreateMarkdownComponent]
+      imports: [MarkdownComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(CreateMarkdownComponent);
+    fixture = TestBed.createComponent(MarkdownComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
